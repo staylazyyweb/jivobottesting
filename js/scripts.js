f71581f34839dd0cdee933afb549b99f8c3bbaae
staylazyyweb <65502110+staylazyyweb@users.noreply.github.com>
2020-11-13 11:09:48 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function jivo_onLoadCallback() {
     console.log('Widget fully loaded');
     jivo_api.showProactiveInvitation("How can I help you?"); 
 }
+
+function clearStorage() {
+    localStorage.clear();
+    window.location.reload();
+}
