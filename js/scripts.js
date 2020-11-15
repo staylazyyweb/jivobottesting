c77c67b2d873e64534b5b5ee7bf3195aed4784d5
staylazyyweb <65502110+staylazyyweb@users.noreply.github.com>
2020-11-15 09:27:21 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,30 @@ function clearStorage() {
     localStorage.clear();
     window.location.reload();
 }
+
+//websocket connection for logs
+let socket = new WebSocket("ws://testbotjivo.eu-gb.cf.appdomain.cloud/ws/dataexchange");
+
+socket.onopen = function(e) {
+  alert("[open] Соединение установлено");
+  alert("Отправляем данные на сервер");
+  socket.send("Меня зовут Джон");
+};
+
+socket.onmessage = function(event) {
+  alert(`[message] Данные получены с сервера: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+  } else {
+    // например, сервер убил процесс или сеть недоступна
+    // обычно в этом случае event.code 1006
+    alert('[close] Соединение прервано');
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
